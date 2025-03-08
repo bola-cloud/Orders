2974320f4c496d105e96d2540aa34ef0e19c7b8c
bola-cloud <bola.ishak41@gmail.com>
2025-03-08 15:32:11 +0200

    update

--- app/Traits/ActivationClass.php
+++ app/Traits/ActivationClass.php
@@ -40,22 +40,22 @@ trait ActivationClass
 
     public function actch()
     {
-        if (!self::is_local()) {
-            $remove = array("http://", "https://", "www.");
-            $url = str_replace($remove, "", url('/'));
+        // if (!self::is_local()) {
+        //     $remove = array("http://", "https://", "www.");
+        //     $url = str_replace($remove, "", url('/'));
 
-            $post = [
-                base64_decode('dXNlcm5hbWU=') => env(base64_decode('QlVZRVJfVVNFUk5BTUU=')),//un
-                base64_decode('cHVyY2hhc2Vfa2V5') => env(base64_decode('UFVSQ0hBU0VfQ09ERQ==')),//pk
-                base64_decode('c29mdHdhcmVfaWQ=') => base64_decode(env(base64_decode('U09GVFdBUkVfSUQ='))),//sid
-                base64_decode('ZG9tYWlu') => $url,
-            ];
-            try {
-                $response = Http::post(base64_decode('aHR0cHM6Ly9jaGVjay42YW10ZWNoLmNvbS9hcGkvdjEvYWN0aXZhdGlvbi1jaGVjaw=='), $post)->json();
-                $status = $response['active'] ?? base64_encode(1);
-                return (int)base64_decode($status);
-            } catch (\Exception $exception) {info($exception->getMessage());}
-        }
+        //     $post = [
+        //         base64_decode('dXNlcm5hbWU=') => env(base64_decode('QlVZRVJfVVNFUk5BTUU=')),//un
+        //         base64_decode('cHVyY2hhc2Vfa2V5') => env(base64_decode('UFVSQ0hBU0VfQ09ERQ==')),//pk
+        //         base64_decode('c29mdHdhcmVfaWQ=') => base64_decode(env(base64_decode('U09GVFdBUkVfSUQ='))),//sid
+        //         base64_decode('ZG9tYWlu') => $url,
+        //     ];
+        //     try {
+        //         $response = Http::post(base64_decode('aHR0cHM6Ly9jaGVjay42YW10ZWNoLmNvbS9hcGkvdjEvYWN0aXZhdGlvbi1jaGVjaw=='), $post)->json();
+        //         $status = $response['active'] ?? base64_encode(1);
+        //         return (int)base64_decode($status);
+        //     } catch (\Exception $exception) {info($exception->getMessage());}
+        // }
         return true;
     }
 
